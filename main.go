80c3aa40ca7fd972d3de6e8306445ecd134c0725
Renish B <renishb10@gmail.com>
2020-06-23 20:18:58 +0530

    displayed feeds for podcast

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/renishb10/golang-podcast/feeds"
 	"github.com/renishb10/golang-podcast/itunes"
 )
 
@@ -19,6 +20,21 @@ func main() {
 		log.Printf("Artist: %s", item.ArtistName)
 		log.Printf("Podcast Name: %s", item.TrackName)
 		log.Printf("Feed URL: %s", item.FeedURL)
+
+		feed, err := feeds.GetFeed(item.FeedURL)
+		if err != nil {
+			log.Fatalf("\tError fetching Feeds: %v", err)
+		}
+
+		for _, pod := range feed.Channel.Item {
+			log.Println("\t----------------------------")
+			log.Printf("\tTitle: %s", pod.Title)
+			log.Printf("\tDuration: %s", pod.Duration)
+			log.Printf("\tDescription: %s", pod.Description)
+			log.Printf("\tURL: %s", pod.Enclosure.URL)
+			log.Println("\t----------------------------")
+		}
+
 		log.Println("-------------------------------")
 	}
 }
